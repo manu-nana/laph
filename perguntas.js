d5051fbd6697db8a655d0720d9a69f4836264d85
manu-nana <136727599+manu-nana@users.noreply.github.com>
2025-06-27 06:17:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'O que causou a revolução francesa?',
+    'As principais causas foram a desigualdade social, a pobreza, a fome, gastos excessivos da monarquia e o iluminismo'
+)
+
+criaCartao(
+    'História',
+    'Em que ano começou e terminou a Segunda Guerra Mundial?',
+    'A Segunda Guerra Mundial começou em 1939 e terminou em 1945'
+)
+
+criaCartao(
+    'História',
+    'O que foi o Estado novo?',
+    'Foi uma ditadura instaurada por Getúlio Vargas, em 1937'
+)
+
+criaCartao(
+    'História',
+    'O que era o feudalismo?',
+    'Um sistema político, social e econômico baseado na posse de terras, com senhores feudais e servos'
+)
+
+criaCartao(
+    'História',
+    'Quem foi Paris e qual foi o seu papel na Guerra de Troia?',
+    'Paris foi um principe troiano, no qual escolheu Afrodite como a deusa mais bela. Em troca, ele recebeu como prêmio o amor da princesa Helena, raptando ela e causando a Guerra de Troia'
+)
+
+criaCartao(
+    'História',
+    'Qual era o regime político da Rússia antes da Revolução Russa?',
+    'Era uma monarquia absolutista, comandada pelo czar Nicolau II, da dinastia Romanov'
+)
