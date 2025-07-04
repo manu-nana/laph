0cbdf9deb4a165d4f0cba62006a6dd5e5f56bb7b
manu-nana <136727599+manu-nana@users.noreply.github.com>
2025-07-03 21:25:31 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,53 @@
 criaCartao(
-    'História',
+    'História!     ',
     'O que causou a revolução francesa?',
     'As principais causas foram a desigualdade social, a pobreza, a fome, gastos excessivos da monarquia e o iluminismo'
 )
 
 criaCartao(
-    'História',
+    'História!',
     'Em que ano começou e terminou a Segunda Guerra Mundial?',
     'A Segunda Guerra Mundial começou em 1939 e terminou em 1945'
 )
 
 criaCartao(
-    'História',
+    'História!',
     'O que foi o Estado novo?',
     'Foi uma ditadura instaurada por Getúlio Vargas, em 1937'
 )
 
 criaCartao(
-    'História',
+    'História!',
     'O que era o feudalismo?',
     'Um sistema político, social e econômico baseado na posse de terras, com senhores feudais e servos'
 )
 
 criaCartao(
-    'História',
+    'História!',
     'Quem foi Paris e qual foi o seu papel na Guerra de Troia?',
     'Paris foi um principe troiano, no qual escolheu Afrodite como a deusa mais bela. Em troca, ele recebeu como prêmio o amor da princesa Helena, raptando ela e causando a Guerra de Troia'
 )
 
 criaCartao(
-    'História',
+    'História!',
     'Qual era o regime político da Rússia antes da Revolução Russa?',
     'Era uma monarquia absolutista, comandada pelo czar Nicolau II, da dinastia Romanov'
+)
+
+criaCartao(
+    'História!',
+    'O que foi a Guerra Fria?',
+    'Foi um conflito ideológico e político entre os Estados Unidos e a União Soviética'
+)
+
+criaCartao(
+    'História!',
+    'O que foi a queda da Bastilha?',
+    'A invasão da prisão da Bastilha em 14 de julho de 1789, marco inicial da Revolução Francesa'
+)
+
+criaCartao(
+    'História!',
+    'Qual foi o tratado que encerrou a Primeira Guerra Mundial?',
+    'Tratado de Versalhes, assinado em 1919, que impôs duras punições à Alemanha'
 )
